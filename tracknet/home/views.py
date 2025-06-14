448ea689e866368eee44933fbaab850ce13a746f
AakashChawla07 <aakashchawla0701@gmail.com>
2025-06-14 16:44:14 +0530

    Remove directory tracknet

--- tracknet/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request,"Home.html")
-
-def progress(request):
-    return render(request,"myprogress.html")
